246aa6c658dacc62621c7e1b576ec54110df55b2
vay-tk <vaytk001@gmail.com>
2025-12-30 10:49:17 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 5173, // Specify the port you want Vite to run on
+    host: true,  // This allows Vite to bind to all network interfaces, including 0.0.0.0
+  }
 });
